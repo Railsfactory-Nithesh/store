2bc9a9c6429b9204e64e509d39abc15c286baf49
Railsfactory-Nithesh <nitheshk@railsfactory.org>
2013-04-29 17:42:56 +0530

    after product category

---
+++ db/migrate/20130429095707_add_to_product.rb
@@ -0,0 +1,9 @@
+class AddToProduct < ActiveRecord::Migration
+  def up
+      add_column :products, :category_id, :integer
+  end
+
+  def down
+      remove_column :products, :category_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130428134416) do
+ActiveRecord::Schema.define(:version => 20130429095707) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(:version => 20130428134416) do
   end
 
   create_table "carts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
@@ -54,6 +60,7 @@ ActiveRecord::Schema.define(:version => 20130428134416) do
     t.datetime "created_at",                                 :null => false
     t.datetime "updated_at",                                 :null => false
     t.string   "product_type"
+    t.integer  "category_id"
   end
 
   create_table "users", :force => true do |t|
